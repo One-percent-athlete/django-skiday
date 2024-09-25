e6ed32afd75506ed476b93861061976f36d960e6
One-percent-athlete <one.percent.athlete@gmail.com>
2024-09-25 15:59:52 +0900

    Setup company info page

--- src/urls.py
+++ src/urls.py
@@ -3,4 +3,5 @@ from src import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('info/', views.info, name="info"),
 ]
--- src/views.py
+++ src/views.py
@@ -4,3 +4,7 @@ import datetime
 def home(request):
     year = datetime.datetime.now().year
     return render(request, 'pages/home.html', {"year":year})
+
+def info(request):
+    year = datetime.datetime.now().year
+    return render(request, 'pages/info.html', {"year":year})
