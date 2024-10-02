6cde68d115f95d39021be15c5899db3d6d9b2e5d
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-02 16:27:25 +0900

    Adjust mt cam page

--- src/urls.py
+++ src/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from src import views
 
 urlpatterns = [
-    # path('jp/', views.home_en, name="home_en"),
-    path('', views.home_jp, name="home"),
+    path('', views.home_en, name="home"),
+    # path('jp/', views.home_jp, name="home_jp"),
     path('info/', views.info, name="info"),
     path('mtcam/', views.mtcam, name="mtcam"),
     path('powderalert/', views.powderalert, name="powderalert"),
     path('matching/', views.matching, name="matching"),
-    path('niseko_en/', views.niseko_en, name="niseko_en"),
-    path('niseko_jp/', views.niseko_jp, name="niseko_jp"),
+    path('niseko/en/', views.niseko_en, name="niseko_en"),
+    path('niseko/jp/', views.niseko_jp, name="niseko_jp"),
 ]
--- src/views.py
+++ src/views.py
@@ -1,34 +1,31 @@
 from django.shortcuts import render
-import datetime
 
-year = datetime.datetime.now().year
 
 def home_en(request):
-    return render(request, 'pages/home_en.html', {"year":year})
+    return render(request, 'home_en.html')
 
-def home_jp(request):
-    return render(request, 'pages/home_jp.html', {"year":year})
+
 
 def info(request):
     
-    return render(request, 'pages/info.html', {"year":year})
+    return render(request, 'info.html')
 
 def mtcam(request):
 
-    return render(request, 'pages/mtcam.html', {"year":year})
+    return render(request, 'mtcam.html')
 
 def powderalert(request):
 
-    return render(request, 'pages/powderalert.html', {"year":year})
+    return render(request, 'powderalert.html')
 
 def matching(request):
 
-    return render(request, 'pages/matching.html', {"year":year})
+    return render(request, 'matching.html')
 
 def niseko_en(request):
 
-    return render(request, 'pages/niseko_en.html', {"year":year})
+    return render(request, 'niseko_en.html')
 
 def niseko_jp(request):
 
-    return render(request, 'pages/niseko_jp.html', {"year":year})
+    return render(request, 'niseko_jp.html')
