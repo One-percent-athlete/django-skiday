af8ea7df4440fd991270e460a2ea4e605f83cea2
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-09 21:55:48 +0900

    Get ready for deploy

--- src/views.py
+++ src/views.py
@@ -2,43 +2,36 @@ from django.shortcuts import render
 from django.contrib import messages
 
 
+def info(request):
+    return render(request, 'info.html')
 
 def home_en(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        company = request.POST["company"]
+        message = request.POST["message"]
+        return render(request, 'home_en.html', {"name": name})
+    return render(request, 'home_en.html')
 
+def mtcam(request):
     if request.method == "POST":
         name = request.POST["name"]
         email = request.POST["email"]
         company = request.POST["company"]
         message = request.POST["message"]
 
-        print(name)
-
-        messages.success(request, 'Thank you for reaching out, we will come back to you later.')
-
-    return render(request, 'home_en.html')
-
-
-
-def info(request):
-    
-    return render(request, 'info.html')
-
-def mtcam(request):
-
+        return render(request, 'mtcam.html' ,{"name": name})
     return render(request, 'mtcam.html')
 
 def powderalert(request):
-
     return render(request, 'powderalert.html')
 
 def matching(request):
-
     return render(request, 'matching.html')
 
 def niseko_en(request):
-
     return render(request, 'niseko_en.html')
 
 def niseko_jp(request):
-
     return render(request, 'niseko_jp.html')
